84b2814d508b07ad409edbaad840ce10d2575661
Servicio-Web <servicioweb22@gmail.com>
2024-07-18 11:41:50 -0700

    respaldo

--- Valmo/settings.py
+++ Valmo/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'NAME': 'valmo',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '192.168.0.108',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
