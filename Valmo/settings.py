66753c6841287c9d7701a4a1a8791a14a61ebc05
Servicio-Web <servicioweb22@gmail.com>
2024-07-22 11:42:41 -0700

    servidos y reportes card!

--- Valmo/settings.py
+++ Valmo/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'valmo',
         'USER': 'root',
-        'PASSWORD': 'geteo393',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
