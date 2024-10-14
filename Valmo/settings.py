dfbd35b252364507ec2f46d7a5c0f106bb3f1197
Servicio-Web <servicioweb22@gmail.com>
2024-10-14 10:25:18 -0700

    settings

--- Valmo/settings.py
+++ Valmo/settings.py
@@ -55,26 +55,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Valmo.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'valmo',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },        
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'Respaldo_MYSQL.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'valmo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },        
     }
 }
+
+# DATABASES = 
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'Respaldo_MYSQL.sqlite3',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
